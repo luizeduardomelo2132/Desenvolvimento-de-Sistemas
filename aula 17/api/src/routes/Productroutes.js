36fdac860eb7ddbb0ad86b3ae45bb11950e26dac
luizeduardo <edulu123419@gmail.com>
2026-08-05 17:20:43 -0300

    adicao da aula 17

---
+++ aula 17/api/src/routes/Productroutes.js
@@ -0,0 +1,124 @@
+import { Router } from "express";
+
+const router = Router();
+
+console.log("Arquivo productRoutes carregado");
+
+const products = [
+    {
+        id: 1,
+        name: "Toyota Corolla",
+        preco: 150000
+    },
+
+    {
+        id: 2,
+        name: "Honda Civic",
+        preco: 160000
+    },
+
+    {
+        id: 3,
+        name: "Chevrolet Onix",
+        preco: 85000
+    },
+
+    {
+        id: 4,
+        name: "Hyundai HB20",
+        preco: 80000
+    },
+
+    {
+        id: 5,
+        name: "Volkswagen Gol",
+        preco: 45000
+    },
+
+    {
+        id: 6,
+        name: "Fiat Uno",
+        preco: 35000
+    },
+
+    {
+        id: 7,
+        name: "Jeep Renegade",
+        preco: 130000
+    },
+
+    {
+        id: 8,
+        name: "Ford Mustang",
+        preco: 500000
+    },
+
+    {
+        id: 9,
+        name: "BMW 320i",
+        preco: 310000
+    },
+
+    {
+        id: 10,
+        name: "Audi A3",
+        preco: 280000
+    },
+
+    {
+        id: 11,
+        name: "Porsche 911",
+        preco: 900000
+    },
+
+    {
+        id: 12,
+        name: "Nissan Kicks",
+        preco: 110000
+    },
+
+    {
+        id: 13,
+        name: "Renault Kwid",
+        preco: 70000
+    },
+
+    {
+        id: 14,
+        name: "Fiat Toro",
+        preco: 140000
+    },
+
+    {
+        id: 15,
+        name: "Toyota Hilux",
+        preco: 260000
+    },
+
+    {
+        id: 16,
+        name: "Chevrolet Tracker",
+        preco: 120000
+    }
+];
+
+
+router.get("/", (req, res) => {
+    console.log("Entrou na rota GET /products");
+
+    res.json(products);
+});
+
+router.post("/", (req, res) => {
+    console.log("Entrou na rota POST /newProducts");
+
+    const newProduct ={  
+        id: products.length + 1,
+        name: req.body.name,
+        preco: req.body.preco
+    };
+
+    res.json(newProduct);
+});
+
+export default router;
---
+++ aula 17/api/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import productsRoutes from "./routes/Productroutes.js"; 
+
+const app = express();
+
+app.set('json spaces', 2);
+
+console.log("ESTE É O SERVER.TS DA TECHSTORE");
+
+const PORT = 3001;
+
+app.use(express.json());
+
+app.use("/products", productsRoutes);
+
+console.log("Rotas de produtos carregadas");
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API está funcionando! "
+    });
+});
+
+app.get("/teste", (req, res) => {
+    res.send("Servidor de teste funcionando!");
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
